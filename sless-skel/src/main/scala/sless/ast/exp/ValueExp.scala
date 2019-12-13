5e03b1ab2b49cbf02bb6a7e191795a4ed84052d7
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2019-12-13 19:56:18 +0100

    Value Aggregation

--- sless-skel/src/main/scala/sless/ast/exp/ValueExp.scala
+++ sless-skel/src/main/scala/sless/ast/exp/ValueExp.scala
@@ -4,26 +4,25 @@ package sless.ast.exp
   * A value expression represents a CSS value. This can be a simple string or a more complex representation.
   */
 sealed trait ValueExp extends Expression {
-  def aggregate(other: ValueExp): ValueExp = this
+  override def pretty(spaces: Int): String = toString + ";"
+  def aggregate(other: ValueExp): ValueExp = ValueStringExp(this.toString + " " + other.toString)
 }
 
-case object ValueInheritExp extends ValueExp { override def pretty(spaces: Int): String = "inherit;" }
-case object ValueInitialExp extends ValueExp { override def pretty(spaces: Int): String = "initial;" }
+case object ValueInheritExp extends ValueExp { override def toString: String = "inherit" }
+case object ValueInitialExp extends ValueExp { override def toString: String = "initial" }
 
-case class ValueStringExp(value: String) extends ValueExp {
-  override def pretty(spaces: Int): String = value + ";"
-  override def aggregate(other: ValueExp): ValueExp = other match {
-    case v: ValueStringExp => ValueStringExp(value + " " + v.value)
-    case _ => super.aggregate(other)
-  }
-}
+case class ValueStringExp(value: String) extends ValueExp { override def toString: String = value }
 
 case class ValueMarginExp(vs: Seq[ValueMarginWidthExp]) extends ValueExp {
-  override def pretty(spaces: Int): String = vs.take(4).map(_.pretty(spaces)).mkString(" ") + ";"
+  override def aggregate(other: ValueExp): ValueExp = other match {
+    case m: ValueMarginExp => ValueMarginExp(vs ++ m.vs)
+    case _ => super.aggregate(other)
+  }
+  override def toString: String = vs.take(4).map(_.toString).mkString(" ")
 }
 
 case class ValueMarginWidthExp(m: Measure) extends ValueExp {
-  override def pretty(spaces: Int): String = m.toString
+  override def toString: String = m.toString
 }
 
 object ValueMarginWidthExp {
